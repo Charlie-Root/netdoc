fe4281337c9e43700879f08e1346000a7df55511
Andrea Dainese <andrea.dainese@pm.me>
2023-01-02 09:54:04 +0100

    update

--- netdoc/discovery/__init__.py
+++
@@ -1,6 +0,0 @@
-__author__     = 'Andrea Dainese'
-__contact__    = 'andrea@adainese.it'
-__copyright__  = 'Copyright 2022, Andrea Dainese'
-__license__    = 'GPLv3'
-__date__       = '2022-11-22'
-__version__    = '0.9.8'
